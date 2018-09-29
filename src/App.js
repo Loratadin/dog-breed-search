503424e2f51db2be90c4b8cc3d7b9afc5ad61b74
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-09-29 16:00:14 -0500

    Import Menu in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Menu from './components/menu/Menu';
 
 class App extends Component {
     render( ) {
         return (
             <div className="App">
+            <Menu/>
             </div>
         );
     }
