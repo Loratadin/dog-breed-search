740bb4bf56d0dbc9536d37e30b0c288077847709
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-09-30 11:09:04 -0500

    Add links and hover effect

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -6,8 +6,8 @@ const Menu = () => {
         <div className="menu-container">
             <div className="menu-logo"><img className="menu-logo-img" src={require('../../assets/logo.png')} alt={'logo'}/></div>
             <div className="menu-title">Breed Finder</div>
-            <div className="menu-github">Source code in Git</div>
-            <div className="menu-twitter">Follow on Twitter</div>
+            <a className="menu-github" href="https://github.com/Loratadin/dog-breed-search" target="_blank" rel="noopener noreferrer">Source code in Git</a>
+            <a className="menu-twitter" href="https://twitter.com/PAIRProgrammin1?lang=en" target="_blank" rel="noopener noreferrer">Follow on Twitter</a>
         </div>
     );
 }
