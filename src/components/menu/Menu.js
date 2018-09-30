04d8ec9e8f2abcb1a6a51a907698fe7b8f2db95b
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-09-30 10:42:55 -0500

    Style the logo

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -4,7 +4,7 @@ import './menu.css';
 const Menu = () => {
     return (
         <div className="menu-container">
-            <div className="menu-logo"></div>
+            <div className="menu-logo"><img className="menu-logo-img" src={require('../../assets/logo.png')} alt={'logo'}/></div>
             <div className="menu-title">Breed Finder</div>
             <div className="menu-github">Source code in Git</div>
             <div className="menu-twitter">Follow on Twitter</div>
