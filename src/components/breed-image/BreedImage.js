ca9c3f7e998bda46e6b51fad034103db96649d67
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-01 07:21:33 -0500

    Create BreedImage Component

---
+++ src/components/breed-image/BreedImage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './breed-image.css';
+
+class BreedImage extends Component {
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default BreedImage;
