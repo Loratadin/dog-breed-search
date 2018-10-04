141e8e2be03d20a151663818fbe1e110d5bbd0c1
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-04 13:09:15 -0500

    Add hamburger menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,11 +3,20 @@ import './menu.css';
 
 const Menu = () => {
     return (
+        <div>
+            <div id={'burger'} className={'burger'} onClick={this.showNav}>
+          <div className={'lines'}>
+            <div className={'line'}></div>
+            <div className={'line'}></div>
+            <div className={'line'}></div>
+          </div>
+        </div>
         <div className="menu-container">
             <div className="menu-logo"><img className="menu-logo-img" src={require('../../assets/logo.png')} alt={'logo'}/></div>
             <div className="menu-title">Breed Finder</div>
             <a className="menu-github" href="https://github.com/Loratadin/dog-breed-search" target="_blank" rel="noopener noreferrer">Source code in Git</a>
             <a className="menu-twitter" href="https://twitter.com/PAIRProgrammin1?lang=en" target="_blank" rel="noopener noreferrer">Follow on Twitter</a>
+        </div>
         </div>
     );
 }
