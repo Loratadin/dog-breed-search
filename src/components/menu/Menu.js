295800053d90e5daf9f48d6861bb4253cfc60d12
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-04 14:37:02 -0500

    Edit responsive styles for mobile menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './menu.css';
 
-const Menu = () => {
-    return (
-        <div>
-            <div id={'burger'} className={'burger'} onClick={this.showNav}>
-          <div className={'lines'}>
-            <div className={'line'}></div>
-            <div className={'line'}></div>
-            <div className={'line'}></div>
-          </div>
-        </div>
-        <div className="menu-container">
-            <div className="menu-logo"><img className="menu-logo-img" src={require('../../assets/logo.png')} alt={'logo'}/></div>
-            <div className="menu-title">Breed Finder</div>
-            <a className="menu-github" href="https://github.com/Loratadin/dog-breed-search" target="_blank" rel="noopener noreferrer">Source code in Git</a>
-            <a className="menu-twitter" href="https://twitter.com/PAIRProgrammin1?lang=en" target="_blank" rel="noopener noreferrer">Follow on Twitter</a>
-        </div>
-        </div>
-    );
-}
+class Menu extends Component {
+    constructor(props) {
+        super(props);
 
+        this.toggleMobileClasses = () => {
+            let hamburgerIcon = document.getElementById('burger');
+            hamburgerIcon.classList.toggle('is-active');
+            let mobileMenu = document.getElementById('menu-container');
+            mobileMenu.classList.toggle('opened');
+        }
+        this.mobile = () => {
+            this.toggleMobileClasses();
+        }
+    }
+        render() {
+            return (
+                <div>
+                    <div id={'burger'} className={'burger'} onClick={this.mobile}>
+                        <div className={'lines'}>
+                            <div className={'line'}></div>
+                            <div className={'line'}></div>
+                            <div className={'line'}></div>
+                        </div>
+                    </div>
+                    <div id={'menu-container'} className="menu-container">
+                        <div className="menu-logo"><img className="menu-logo-img" src={require('../../assets/logo.png')} alt={'logo'} /></div>
+                        <div className="menu-title">Breed Finder</div>
+                        <a className="menu-github" href="https://github.com/Loratadin/dog-breed-search" target="_blank" rel="noopener noreferrer">Source code in Git</a>
+                        <a className="menu-twitter" href="https://twitter.com/PAIRProgrammin1?lang=en" target="_blank" rel="noopener noreferrer">Follow on Twitter</a>
+                    </div>
+                </div>
+            );
+        }
+    }
 export default Menu;
