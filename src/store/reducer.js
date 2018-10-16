0cc8b28bdea8f994b34a1df49264ecb933e16553
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-16 15:42:44 -0500

    Set up the reducer and import it into index.js

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    breedsList: null,
+    selectedBreed: null,
+    error: false,
+    imageUrl: require('../../assets/custom.jpg'),
+    selectedBreed: null
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+}
+
+export default reducer;
